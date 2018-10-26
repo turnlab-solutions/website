2e2b8f03e162013a411fd8c20b1c20de31a75b9a
Maxime Letourneur <maxime.letourneur@gmail.com>
2018-10-26 16:52:58 -0700

    Log day 4

--- src/pages/100-days-of-code-challenge.js
+++ src/pages/100-days-of-code-challenge.js
@@ -19,9 +19,15 @@ const SecondPage = () => (
     </p>
     <h3>Day 3 - October 25, Thursday, 2018</h3>
     <p>
-    <strong>Today's Progress</strong>: Made a JS clockface day for 2 of Javascript 30<br />
+    <strong>Today's Progress</strong>: Made a JS clockface day for #2 of Javascript 30<br />
     <strong>Thoughts:</strong> Reminded me that CSS transform/animations are the fundations for a UI where nothing is created, everything is transformed from one view to the next...<br />
     <a href="https://maxletourneur.github.io/JavaScript30/02%20-%20JS%20and%20CSS%20Clock/">Link to work</a><br />
+    </p>
+    <h3>Day 4 - October 26, Friday, 2018</h3>
+    <p>
+    <strong>Today's Progress</strong>: Played with CSS variables and image filter for #3 of Javascript 30<br />
+    <strong>Thoughts:</strong>HTML color picker is still not supported on iOS (displays as input text) Shout out to Android for handling it beautifully!<br />
+    <a href="https://maxletourneur.github.io/JavaScript30/03%20-%20CSS%20Variables/">Link to work</a><br />
     </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
