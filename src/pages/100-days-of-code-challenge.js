a692e0da448657143c1b75433890dc6e3f797d30
Maxime Letourneur <maxime.letourneur@gmail.com>
2018-10-25 21:13:49 -0700

    Log day 3

--- src/pages/100-days-of-code-challenge.js
+++ src/pages/100-days-of-code-challenge.js
@@ -9,13 +9,19 @@ const SecondPage = () => (
     <h3>Day 1 - October 23, Tuesday, 2018</h3>
     <p><strong>Today's Progress</strong>: Deploy Gatsby Website for my Turnlab Solutions website (portfolio) using Netlify<br />
     <strong>Thoughts:</strong> For the first 30 days I'll do Javascript 30 and publish my projects on this website<br />
-    <strong>Link to work:</strong> <a href="https://turnlab.solutions">https://turnlab.solutions</a><br />
+    <a href="https://turnlab.solutions">Link to work</a><br />
     </p>
-    <h3>Day 2 - October 24, Tuesday, 2018</h3>
+    <h3>Day 2 - October 24, Wednesday, 2018</h3>
     <p>
     <strong>Today's Progress</strong>: Did the first challenge of Javascript 30<br />
     <strong>Thoughts:</strong> That was fun I had never needed the audio html element before<br />
-    <strong>Link to work:</strong> <a href="https://maxletourneur.github.io/JavaScript30/01%20-%20JavaScript%20Drum%20Kit/">https://maxletourneur.github.io/JavaScript30/01%20-%20JavaScript%20Drum%20Kit/</a><br />
+    <a href="https://maxletourneur.github.io/JavaScript30/01%20-%20JavaScript%20Drum%20Kit/">Link to work</a><br />
+    </p>
+    <h3>Day 3 - October 25, Thursday, 2018</h3>
+    <p>
+    <strong>Today's Progress</strong>: Made a JS clockface day for 2 of Javascript 30<br />
+    <strong>Thoughts:</strong> Reminded me that CSS transform/animations are the fundations for a UI where nothing is created, everything is transformed from one view to the next...<br />
+    <a href="https://maxletourneur.github.io/JavaScript30/02%20-%20JS%20and%20CSS%20Clock/">Link to work</a><br />
     </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
